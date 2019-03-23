8837febdac4d449f2193f3fb1eb96b2ef97bfe6a
Arnt-Helge Nilsen Øyan <arhenioy@gmail.com>
2019-03-23 19:17:25 +0100

    Module #3 completed

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 namespace GradeBook.GradeBooks
 {
@@ -31,6 +32,26 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else
                 return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+
+            base.CalculateStudentStatistics(name);
         }
     }
 }
