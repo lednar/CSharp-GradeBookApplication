fdcb617e9a3d9655f9d324273da7bed672049320
Arnt-Helge Nilsen Øyan <arhenioy@gmail.com>
2019-03-23 19:20:02 +0100

    Corrected
    Wasn't suppose to throw an exception, just a console writeline. Read the damn task.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -38,7 +38,7 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
             {
-                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
             }
 
             base.CalculateStatistics();
@@ -48,7 +48,7 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
             {
-                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
             }
 
             base.CalculateStudentStatistics(name);
